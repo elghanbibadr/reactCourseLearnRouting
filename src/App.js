69f80c082b85bb5a4df13f7a3796ca46cb94b16c
badr ghanbi <bghanbi50@gmail.com>
2023-01-14 15:51:15 +0100

    add the router componenet

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React from "react";
 import { Route } from "react-router-dom";
+import Welcome from "./component/Welcome";
+import Products from "./component/Products";
 
 function App() {
   return (
     <div>
-      <h2>Let's get started</h2>
+      <Route path="/welcome" >
+        <Welcome />
+      </Route>
+      <Route path="/products" >
+        <Products/>
+      </Route>
     </div>
   );
 }
--- src/component/Welcome.js
+++ src/component/Welcome.js
@@ -2,7 +2,10 @@ import React from 'react'
 
 const Welcome = () => {
   return (
+    <>
     <h1>Welcome Page</h1>
+    <p>hi</p>
+    </>
   )
 }
 
