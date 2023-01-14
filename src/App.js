2234d0ca1fa8aab5ae81905c45f567ad52bf9fa3
badr ghanbi <bghanbi50@gmail.com>
2023-01-14 15:44:36 +0100

    'set up work envirennement'

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Route } from "react-router-dom";
+
+function App() {
+  return (
+    <div>
+      <h2>Let's get started</h2>
+    </div>
+  );
+}
+
+export default App;
